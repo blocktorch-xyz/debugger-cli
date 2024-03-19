7e712b9fc51e266131c15198150d1dbc35c70854
iuwqyir <toomas.oosalu@gmail.com>
2024-03-19 10:49:00 +0000

    build: use typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
-import open from "open";
+import { Command } from 'commander';
+import open from 'open';
+
+const program = new Command();
 
 const BLOCKTORCH_DEBUGGER_URL = 'https://app.blocktorch.xyz/explorer/a6b7b927-934f-4f93-b504-864da065bc0c/0x762583066127e123938aaef11097220efa137a49f836ae98d3b78d8c9fa86a72?type=transaction&debugger=open';
 
